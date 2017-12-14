ddbe5d8259ebacdb70ced20d8eadccf599a6c02d
Nicolas Dao <nic@neap.co>
2017-12-14 11:46:59 +1100

    Update index.js

--- templates/index.js
+++ templates/index.js
@@ -1,4 +1,4 @@
-const { listen, serveHttp, app } = require('webfunc')
+const { listen, serve, app } = require('webfunc')
 
 /**
  * Responds to any HTTP request.
@@ -6,7 +6,7 @@ const { listen, serveHttp, app } = require('webfunc')
  * @param {!Object} req Cloud Function request context.
  * @param {!Object} res Cloud Function response context.
  */
-const server = serveHttp([
+const server = serve([
 	app.get('/', (req, res) => res.status(200).send('Hello World')),
 	app.get('/users/:userId', (req, res, params) => res.status(200).send(`Hello user ${params.userId}`)),
 	app.get('/users/:userId/document/:docName', (req, res, params) => res.status(200).send(`Hello user ${params.userId}. I like your document ${params.docName}`)),
